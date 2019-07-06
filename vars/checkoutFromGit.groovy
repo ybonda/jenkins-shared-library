1f5d1b41671f309e9b1942e7651d7e9bc6af7a70
Yuri Bondarenko <ybonda@gmail.com>
2019-07-06 11:00:12 +0300

    ...

--- vars/checkoutFromGit.groovy
+++ vars/checkoutFromGit.groovy
@@ -4,16 +4,18 @@ def call(String repoName, String branch) {
     if (repoName == null || branch == null)
         throw new RuntimeException("Cannot fetch repo!")
 
-    withEnv(["REPO_NAME=${repoName}", "BRANCH=${branch}"]) {
-        sh '''
-       set +e
-	   echo "INFO Checking out repo: ${REPO_NAME}"
-	   echo "INFO Branch:            ${BRANCH}"
-	   
-	   git branch: '${BRANCH}', changelog: false, poll: false, url: '${REPO_NAME}'
-       
-      '''
-    }
+    git branch: '${BRANCH}', changelog: false, poll: false, url: '${REPO_NAME}'
+
+//    withEnv(["REPO_NAME=${repoName}", "BRANCH=${branch}"]) {
+//        sh '''
+//       set +e
+//	   echo "INFO Checking out repo: ${REPO_NAME}"
+//	   echo "INFO Branch:            ${BRANCH}"
+//
+//	   git branch: '${BRANCH}', changelog: false, poll: false, url: '${REPO_NAME}'
+//
+//      '''
+//    }
 
 }
 
